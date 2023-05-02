0ff534f61787e21cba3a49ac12776eeff09c309c
Tevfik <tevfiksarlak@gmail.com>
2023-05-03 01:14:35 +0300

    javacript

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ function navToggle() {
 }
 
 var typed = new Typed(".mov",{
-    strings: [,  "Mathematician","Software Developer","Blockchain Enthusiast","lifelong learner","interested in Cryptography"] ,
+    strings: [,  "Mathematician","Software Developer","Blockchain Enthusiast","lifelong learner",
+                 "interested in Cryptography"] ,
     typeSpeed: 60,
     backSpeed: 60,
     loop: true
@@ -25,4 +26,101 @@ var typed = new Typed(".mov",{
       // clear the form fields
       form.reset();
     });
+
+    function ToggleTextOne() {
+      let dots = document.getElementById("dots-one");
+      let moreText = document.getElementById("moretext-one");
+      let buttonReadmore = document.getElementById("btn-readmore-one");
+      let buttonReadless = document.getElementById("btn-readless-one");
+
+    
+      if (!dots.classList.contains("hidden")) {
+        dots.classList.add("hidden");
+        moreText.classList.remove("hidden");
+        buttonReadmore.classList.add("hidden");
+        buttonReadless.classList.remove("hidden");
+      } else {
+        dots.classList.remove("hidden");
+        moreText.classList.add("hidden");
+        buttonReadmore.classList.remove("hidden");
+        buttonReadless.classList.add("hidden");
+      }
+    }
+
+    function ToggleTextTwo() {
+      let dots = document.getElementById("dots-two");
+      let moreText = document.getElementById("moretext-two");
+      let buttonReadmore = document.getElementById("btn-readmore-two");
+      let buttonReadless = document.getElementById("btn-readless-two");
+    
+      if (!dots.classList.contains("hidden")) {
+        dots.classList.add("hidden");
+        moreText.classList.remove("hidden");
+        buttonReadmore.classList.add("hidden");
+        buttonReadless.classList.remove("hidden");
+      } else {
+        dots.classList.remove("hidden");
+        moreText.classList.add("hidden");
+        buttonReadmore.classList.remove("hidden");
+        buttonReadless.classList.add("hidden");
+      }
+    }
+
+    function ToggleTextThree() {
+      let dots = document.getElementById("dots-three");
+      let moreText = document.getElementById("moretext-three");
+      let buttonReadmore = document.getElementById("btn-readmore-three");
+      let buttonReadless = document.getElementById("btn-readless-three");
+    
+      if (!dots.classList.contains("hidden")) {
+        dots.classList.add("hidden");
+        moreText.classList.remove("hidden");
+        buttonReadmore.classList.add("hidden");
+        buttonReadless.classList.remove("hidden");
+      } else {
+        dots.classList.remove("hidden");
+        moreText.classList.add("hidden");
+        buttonReadmore.classList.remove("hidden");
+        buttonReadless.classList.add("hidden");
+      }
+    }
+    
+    function ToggleTextFour() {
+      let dots = document.getElementById("dots-four");
+      let moreText = document.getElementById("moretext-four");
+      let buttonReadmore = document.getElementById("btn-readmore-four");
+      let buttonReadless = document.getElementById("btn-readless-four");
+    
+      if (!dots.classList.contains("hidden")) {
+        dots.classList.add("hidden");
+        moreText.classList.remove("hidden");
+        buttonReadmore.classList.add("hidden");
+        buttonReadless.classList.remove("hidden");
+      } else {
+        dots.classList.remove("hidden");
+        moreText.classList.add("hidden");
+        buttonReadmore.classList.remove("hidden");
+        buttonReadless.classList.add("hidden");
+      }
+    }
+
+    function ToggleTextFive() {
+      let dots = document.getElementById("dots-five");
+      let moreText = document.getElementById("moretext-five");
+      let buttonReadmore = document.getElementById("btn-readmore-five");
+      let buttonReadless = document.getElementById("btn-readless-five");
+    
+      if (!dots.classList.contains("hidden")) {
+        dots.classList.add("hidden");
+        moreText.classList.remove("hidden");
+        buttonReadmore.classList.add("hidden");
+        buttonReadless.classList.remove("hidden");
+      } else {
+        dots.classList.remove("hidden");
+        moreText.classList.add("hidden");
+        buttonReadmore.classList.remove("hidden");
+        buttonReadless.classList.add("hidden");
+      }
+    }
+    
     
